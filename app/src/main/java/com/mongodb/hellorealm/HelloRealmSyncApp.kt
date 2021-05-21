7c8af050f107bf95cda9ec8128843657996995d4
Mohit Sharma <mohit7575@gmail.com>
2021-05-21 12:51:07 +0100

    class renamed

---
+++ app/src/main/java/com/mongodb/hellorealm/HelloRealmSyncApp.kt
@@ -0,0 +1,19 @@
+package com.mongodb.hellorealm
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+
+class HelloRealmSyncApp : Application() {
+
+    val realmSync by lazy {
+        App(AppConfiguration.Builder(BuildConfig.RealmAppId).build())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+
+}
