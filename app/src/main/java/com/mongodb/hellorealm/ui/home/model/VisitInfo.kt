aede07b3b0d14c016e3569a7bb43a76af6253e11
Mohit Sharma <mohit7575@gmail.com>
2021-05-26 11:05:13 +0100

    key mapping updated

--- app/src/main/java/com/mongodb/hellorealm/ui/home/model/VisitInfo.kt
+++ app/src/main/java/com/mongodb/hellorealm/ui/home/model/VisitInfo.kt
@@ -2,12 +2,14 @@ package com.mongodb.hellorealm.ui.home.model
 
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmField
 import java.util.*
 
 open class VisitInfo : RealmObject() {
 
     @PrimaryKey
-    var _id: String = UUID.randomUUID().toString()
+    @RealmField("_id")
+    var id: String = UUID.randomUUID().toString()
 
     var visitCount: Int = 0
 
